b8b6a6d9aab82e551b78e98f0756e8e33c93501a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-26 22:00:24 +0000

    move back code to proper place
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.externalization/trunk@93129 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/externalization/tests/test_integer_strings.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+import sys
+import unittest
+
+from nti.externalization import integer_strings
+
+class TestIntStrings(unittest.TestCase):
+
+	def test_round_trip(self):
+
+		def _t(i):
+			ext = integer_strings.to_external_string(i)
+			__traceback_info__ = i, ext
+			parsed = integer_strings.from_external_string(ext)
+			assert_that(parsed, is_(i))
+
+		# Small values
+		for i in range(0, 100):
+			_t(i)
+
+		# Medium values
+		for i in range(2000, 5000):
+			_t(i)
+
+		# Big values
+		for i in range(sys.maxint - 2000, sys.maxint):
+			_t(i)
+
+	def test_decode_unicode(self):
+		assert_that(integer_strings.from_external_string(u'abcde'), is_(204869188))
