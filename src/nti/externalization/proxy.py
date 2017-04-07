f326e77f69274c867eea08d48086ddf4d1ffd25f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-07 22:33:19 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.externalization/trunk@110397 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/externalization/proxy.py
+++ src/nti/externalization/proxy.py
@@ -13,44 +13,45 @@ logger = __import__('logging').getLogger(__name__)
 
 _unwrappers = []
 try:
-	import zope.proxy
-	_unwrappers.append(zope.proxy.removeAllProxies)
+    import zope.proxy
+    _unwrappers.append(zope.proxy.removeAllProxies)
 except ImportError:
-	pass
+    pass
 
 try:
-	import zope.container.contained
-	_unwrappers.append(zope.container.contained.getProxiedObject)
+    import zope.container.contained
+    _unwrappers.append(zope.container.contained.getProxiedObject)
 except ImportError:
-	pass
+    pass
 
 try:
-	import Acquisition
-	aq_base = getattr(Acquisition, 'aq_base')
-	_unwrappers.append(aq_base)
+    import Acquisition
+    aq_base = getattr(Acquisition, 'aq_base')
+    _unwrappers.append(aq_base)
 except (ImportError, AttributeError):
-	pass
+    pass
+
 
 def removeAllProxies(proxy):
-	"""
-	If the object in ``proxy`` is proxied by one of the types
-	of proxies known about by this module, remove all of the
-	known proxies, unwrapping down to the original base object.
+    """
+    If the object in ``proxy`` is proxied by one of the types
+    of proxies known about by this module, remove all of the
+    known proxies, unwrapping down to the original base object.
 
-	This module may know about :mod:`zope.proxy`, :mod:`zope.container.contained`,
-	and :mod:`Acquisition`.
-	"""
+    This module may know about :mod:`zope.proxy`, :mod:`zope.container.contained`,
+    and :mod:`Acquisition`.
+    """
 
-	fully_unwrapped_by = [False] * len(_unwrappers)
+    fully_unwrapped_by = [False] * len(_unwrappers)
 
-	while not all(fully_unwrapped_by):
-		for i, unwrapper in enumerate(_unwrappers):
-			unwrapped = unwrapper(proxy)
-			if unwrapped is not proxy:
-				# It changed something. reset the whole state.
-				fully_unwrapped_by[:] = [False] * len(_unwrappers)
-				proxy = unwrapped
-			else:
-				# yes, this one is completely done
-				fully_unwrapped_by[i] = True
-	return proxy
+    while not all(fully_unwrapped_by):
+        for i, unwrapper in enumerate(_unwrappers):
+            unwrapped = unwrapper(proxy)
+            if unwrapped is not proxy:
+                # It changed something. reset the whole state.
+                fully_unwrapped_by[:] = [False] * len(_unwrappers)
+                proxy = unwrapped
+            else:
+                # yes, this one is completely done
+                fully_unwrapped_by[i] = True
+    return proxy
