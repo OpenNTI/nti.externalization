6b16bb9486583e8ae830058a4da0fa7531407a1b
Carlos Sanchez <papachoco@gmail.com>
2017-08-18 17:21:45 -0500

    remove unicode literals

--- src/nti/externalization/__init__.py
+++ src/nti/externalization/__init__.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
