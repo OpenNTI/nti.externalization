ebbcc5797a588319ef9219a4ae5728fb89f47d41
Carlos Sanchez <papachoco@gmail.com>
2017-08-18 23:03:27 -0500

    pep8

--- src/nti/externalization/tests/test_integer_strings.py
+++ src/nti/externalization/tests/test_integer_strings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
@@ -13,29 +13,32 @@ from hamcrest import assert_that
 import sys
 import unittest
 
-from nti.externalization import integer_strings
+from nti.externalization.integer_strings import to_external_string
+from nti.externalization.integer_strings import from_external_string
+
 
 class TestIntStrings(unittest.TestCase):
 
-	def test_round_trip(self):
+    def test_round_trip(self):
 
-		def _t(i):
-			ext = integer_strings.to_external_string(i)
-			__traceback_info__ = i, ext
-			parsed = integer_strings.from_external_string(ext)
-			assert_that(parsed, is_(i))
+        def _t(i):
+            ext = to_external_string(i)
+            __traceback_info__ = i, ext
+            parsed = from_external_string(ext)
+            assert_that(parsed, is_(i))
 
-		# Small values
-		for i in range(0, 100):
-			_t(i)
+        # Small values
+        for i in range(0, 100):
+            _t(i)
 
-		# Medium values
-		for i in range(2000, 5000):
-			_t(i)
+        # Medium values
+        for i in range(2000, 5000):
+            _t(i)
 
-		# Big values
-		for i in range(sys.maxint - 2000, sys.maxint):
-			_t(i)
+        # Big values
+        for i in range(sys.maxint - 2000, sys.maxint):
+            _t(i)
 
-	def test_decode_unicode(self):
-		assert_that(integer_strings.from_external_string(u'abcde'), is_(204869188))
+    def test_decode_unicode(self):
+        assert_that(from_external_string(u'abcde'), 
+					is_(204869188))
