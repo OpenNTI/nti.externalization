1593a4aff60f6ea8baf85a684c58c43d2b17f6a7
Jason Madden <jamadden@gmail.com>
2026-05-08 14:01:34 -0500

    Back to development: 3.1.1

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ if not PYPY:
 
 setup(
     name='nti.externalization',
-    version='3.1.0',
+    version='3.1.1.dev0',
     author='Jason Madden',
     author_email='jason@seecoresoftware.com',
     description="NTI Externalization",
